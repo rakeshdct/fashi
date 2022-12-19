411285efafc7fdc6de6f0fd5b73b809da4998015
Rakesh <rakesh.v6@cognizant.com>
2022-12-19 18:32:13 +0530

    faq page content changed to next js

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../styles/faq.module.css'
+import { useState } from "react";
 
 const Faq = () => {
     return (
@@ -8,52 +9,19 @@ const Faq = () => {
                 <div className="row">
                     <div className="col-lg-12">
                         <div className={styles.faqAccordin}>
-                            <div className={styles.accordion} id="accordionExample">
-                                <div className={styles.card}>
-                                    <div className={[styles.cardHeading, styles.active].join(" ")}>
-                                        <a className="active" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true">
-                                            Is There Anything I Should Bring?
-                                        </a>
-                                    </div>
-                                    <div id="collapseOne" className={[styles.collapse, styles.show].join(" ")} data-parent="#accordionExample">
-                                        <div className={styles.cardBody}>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className={styles.card}>
-                                    <div className={styles.cardHeading}>
-                                        <a data-toggle="collapse" data-target="#collapseTwo">
-                                            Where Can I Find Market Research Reports?
-                                        </a>
-                                    </div>
-                                    <div id="collapseTwo" className={styles.collapse} data-parent="#accordionExample">
-                                        <div className={styles.cardBody}>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className={styles.card}>
-                                    <div className={styles.cardHeading}>
-                                        <a data-toggle="collapse" data-target="#collapseThree">
-                                            Where Can I Find The Wall Street Journal?
-                                        </a>
-                                    </div>
-                                    <div id="collapseThree" className={styles.collapse} data-parent="#accordionExample">
-                                        <div className={styles.cardBody}>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat.</p>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className={styles.accordion} id="accordionExample">    
+                                <Faqqanda question="Is There Anything I Should Bring?" answer="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat."/>
+                                <Faqqanda question="Where Can I Find Market Research Reports?" answer="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat."/>          
+                                <Faqqanda question="Where Can I Find The Wall Street Journal?" answer="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat."/>                                                                          
                             </div>
                         </div>
                     </div>
@@ -64,3 +32,27 @@ const Faq = () => {
 }
 
 export default Faq
+
+export function Faqqanda(props) {
+    const [isShowing, setIsShowing] = useState(false);
+
+    const toggle = () => {
+      setIsShowing(!isShowing);
+    };    
+    return (
+        <>
+            <div className={styles.card}>
+                <div className={styles.cardHeading}>
+                    <a data-toggle="collapse" onClick={toggle} data-target="#collapseOne" aria-expanded={isShowing}>
+                       {props.question} 
+                    </a>
+                </div>
+                <div id="collapseOne" className={[styles.collapse, styles.show].join(" ")} data-parent="#accordionExample" style={{ display: isShowing ? "block" : "none" }}>
+                    <div className={styles.cardBody}>
+                        <p>{props.answer}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
